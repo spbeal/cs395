bd867a85c7484d7ae12635c788672b1b568e7c5c
Samuel Beal <sampbeal@gmail.com>
2025-03-08 00:45:44 -0800

    w15

--- src/w15.c
+++ src/w15.c
@@ -1,31 +1,148 @@
 /*********************************************
- * Id: beal6782
- * 
- * Russian Peasant Multiplication
- * 
- * Implement an algorithm for russian peasant multiplcation
- * n/2 * 2m for even
- * n-1/2 * 2m + m for odd
- *********************************************/ 
+* Id: beal6782
+* 
+* Boyer-Moore algorithm
+* 
+* Implement an algorithm for Boyer-Moore
+* Use w14 for ShiftTable
+*********************************************/ 
 #include <stdio.h>
 #include <stdlib.h>
-//#include <string.h>
+#include <string.h>
+#include <stdbool.h>
+ 
+#define MAX_ALPHABET 128
+#define TABLE_ROW_LENGTH 16
+#define MIN_WRITEABLE 32
 
-void multiply(int n, int m);
-void printMul(int n, int m);
-
-int main (int argc, char *argv[])
+void printShiftTable(int table[])
 {
-   if (argc < 3)
+   int i, start;
+   for(start = MIN_WRITEABLE; start < MAX_ALPHABET; start+=TABLE_ROW_LENGTH)
    {
-      printf("%s 1 2 3 4 5 6 7 8", argv[0]);
-      exit(1);
-   }
+      for(i = start; i < start+TABLE_ROW_LENGTH && i < MAX_ALPHABET; i++)
+      {
+         printf("%c\t", i);
+      }
+      printf("\n");
 
-   int x = atoi(argv[1]);
-   int y = atoi(argv[2]);
-   multiply(x,y);
+      for(i = start; i < start+TABLE_ROW_LENGTH && i < MAX_ALPHABET; i++)
+      {
+         printf("%d\t", table[i]);
+      }
+      printf("\n\n");
+
+   }
 }
+
+// Horspools
+int * ShiftTable(const char * needle, int m){
+   int * Table = malloc(MAX_ALPHABET * sizeof(int));
+   for (int i = 0; i < MAX_ALPHABET; i++) Table[i] = m;
+   for (int j = 0; j < m-1; j++) Table[(unsigned char)needle[j]] = m - 1 - j;
+   printShiftTable(Table);
+   return Table;
+}
+
+////////////////////////////////
+////////////////////////////////
+
+bool equal(const char *pattern, int start1, int start2, int len) {
+   for (int i = 0; i < len; i++) {
+      if (pattern[start1 + i] != pattern[start2 + i]) return false;
+   }
+   return true;
+}
+
+int *GoodSuffixTable(const char *needle, int *gsuffix, int m) {
+   for (int k = 1; k < m; k++) {
+      bool found = false;
+      for (int j = m - k - 1; j >= 0; j--) {
+         if (equal(needle, j, m - k, k) && 
+               (j == 0 || needle[j - 1] != needle[m - k - 1])) {
+               gsuffix[k] = m - k - j;
+               found = true;
+               break;
+         }
+      }
+      if (!found) {
+         for (int l = k; l >= 1; l--) {
+               if (equal(needle, 0, m - l, l)) {
+                  gsuffix[k] = m - l;
+                  break;
+               }
+               if (l == 1) gsuffix[k] = m;
+         }
+      }
+   }
+   for (int i = 1; i < m; i++) {
+      printf("%d %*s %d\n", i, m, &needle[m - i], gsuffix[i]);
+   }
+   return gsuffix;
+}
+
+////////////////////////////////
+////////////////////////////////
+
+void boyer_moore_search(const char *haystack, const char *needle) {
+   int n = strlen(haystack); int m = strlen(needle); if (m == 0) return;
+
+   int * bad_symbol = ShiftTable(needle, m);
+   int gsuffix[m]; GoodSuffixTable(needle, gsuffix, m);
+   int matchNum = 0;
+   int * matches = (int * )malloc(n*sizeof(int));
+
+   printf("%s\n", haystack); // print out string
+
+   /////////////////////
+   // Engine
+   int s = 0;
+   while (s <= n - m) {
+      int j = m - 1;
+      while (j >= 0 && needle[j] == haystack[s + j]) j--;
+
+      printf("%*s%s", s, "", needle);
+      if (j < 0) {
+         printf("---found");
+         matches[matchNum++] = s;
+         s += 1;
+      } else {
+         int k = m - 1 - j;
+         char c = haystack[s + j];
+
+         int d1 = bad_symbol[(unsigned char)c] - k; if (d1 < 1) d1 = 1;         
+         int d2 = (k > 0) ? gsuffix[k] : 0;
+         int d = (k == 0) ? d1 : (d1 > d2 ? d1 : d2);
+         // d1, d2
+         if (k > 0 && k < m)
+            printf(" d1=%d d2=%d", d1, d2);
+         s += d;
+      }
+      printf("\n");
+   }
+   /////////////////////
+
+   printf("Matches found at locations:");
+   for(int i = 0; i < matchNum; i++)
+   {
+      printf(" %d", matches[i]);
+   }
+   printf("\n");
+}
+
+////////////////////////////////
+////////////////////////////////
+
+int main(int argc, char *argv[]) {
+   if (argc != 3) {
+      printf("Usage: %s <pattern> <text>\n", argv[0]);
+      return 1;
+   }
+   // Haystack, needle
+   boyer_moore_search(argv[2], argv[1]);
+   return 0;
+}
+
 /*
 Boyer-Moore algorithm
 
