c135ae76bc4c395faed317e1521067ab672b3c76
Samuel Beal <sampbeal@gmail.com>
2025-03-09 14:30:00 -0700

    w16

--- src/w16.c
+++ src/w16.c
@@ -1,31 +1,97 @@
 /*********************************************
  * Id: beal6782
  * 
- * Russian Peasant Multiplication
+ * Quick Hull
  * 
- * Implement an algorithm for russian peasant multiplcation
- * n/2 * 2m for even
- * n-1/2 * 2m + m for odd
+ * Implement an algorithm for finding a Convex Hull
  *********************************************/ 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void multiply(int n, int m);
-void printMul(int n, int m);
+typedef struct Point {
+   int x;
+   int y;
+} Point;
+
+// find the side of a point relative to a line
+int find_side(Point p1, Point p2, Point p) {
+   int val = (p.y - p1.y) * (p2.x - p1.x) - (p2.y - p1.y) * (p.x - p1.x);
+   return (val > 0) - (val < 0);
+}
+
+// find the distance from a point to a line
+int distance(Point p1, Point p2, Point p) {
+   return abs((p.y - p1.y) * (p2.x - p1.x) - (p2.y - p1.y) * (p.x - p1.x));
+}
+
+void quick_hull(Point points[], int n, Point p1, Point p2, int side, Point hull[], int *hull_size) {
+   int max_dist = 0, index = -1;
+   for (int i = 0; i < n; i++) {
+       int dist = distance(p1, p2, points[i]);
+       if (find_side(p1, p2, points[i]) == side && dist > max_dist) {
+           max_dist = dist;
+           index = i;
+       }
+   }
+
+   if (index == -1) {
+       hull[(*hull_size)++] = p1;
+       return;
+   }
+
+   quick_hull(points, n, points[index], p1, -find_side(points[index], p1, p2), hull, hull_size);
+   quick_hull(points, n, points[index], p2, -find_side(points[index], p2, p1), hull, hull_size);
+}
+
+void convex_hull(Point points[], int n) {
+   if (n < 3) {
+       printf("Convex hull not possible\n");
+       return;
+   }
+   
+   int min_index = 0, max_index = 0;
+   for (int i = 1; i < n; i++) {
+       if (points[i].x < points[min_index].x || (points[i].x == points[min_index].x && points[i].y < points[min_index].y))
+         min_index = i;
+       if (points[i].x > points[max_index].x || (points[i].x == points[max_index].x && points[i].y > points[max_index].y))
+         max_index = i;
+   }
+   
+   Point hull[n];
+   int hull_size = 0;
+   
+   quick_hull(points, n, points[min_index], points[max_index], 1, hull, &hull_size);
+   quick_hull(points, n, points[min_index], points[max_index], -1, hull, &hull_size);
+   
+   // Sort points in clockwise order
+   printf("The points in Convex Hull are:\n");
+   for (int i = 0; i < hull_size; i++) {
+       printf("(%d, %d) ", hull[i].x, hull[i].y);
+   }
+   printf("(%d, %d)\n", hull[0].x, hull[0].y);
+}
 
 int main (int argc, char *argv[])
 {
-   if (argc < 3)
+   if (argc < 5)
    {
       printf("%s 1 2 3 4 5 6 7 8", argv[0]);
       exit(1);
    }
 
-   int x = atoi(argv[1]);
-   int y = atoi(argv[2]);
-   multiply(x,y);
+   int n = (argc - 1) / 2;
+   Point points[n];
+   for (int i = 0; i < n; i++)
+   {
+      points[i].x = atoi(argv[i*2 + 1]);
+      points[i].y = atoi(argv[i*2 + 2]); // not out of bounds because argc is n+1
+      // printf("%d %d ", points[i].x, points[i].y);
+   }
+   convex_hull(points, n);
 }
+   // memset(convex_hull, 0, half_n);
+
 /*
 Quick Hull 
 
