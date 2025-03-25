ea851301de9eb67f2f02328c41e6611dbf488043
Samuel Beal <sampbeal@gmail.com>
2025-03-24 18:15:44 -0700

    w19

--- src/w19.c
+++ src/w19.c
@@ -9,38 +9,196 @@
 #include <stdlib.h>
 #include <string.h>
 
-void engine( int x, int y, int n, char arr[])
-{
+#define MAX_BOARD 100
 
+int V, U, start, end;
+int G[MAX_BOARD][MAX_BOARD];
+int matchV[MAX_BOARD], matchU[MAX_BOARD];
+int labelV[MAX_BOARD], labelU[MAX_BOARD];
+int queue[MAX_BOARD * 2];
+
+int M[MAX_BOARD];
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+
+// Print out the currenlty found matchings
+// void printM(int start, int end) {
+//    int i;
+//    for(i=start; i<end; i++)
+//    {
+//       printf("\tV%d", M[i][0]);
+//    }
+//    printf("\n");
+
+//    for(i=start; i<end; i++)
+//    {
+//       printf("\t|");
+//    }
+//    printf("\n");
+
+//    for(i=start; i<end; i++)
+//    {
+//       printf("\tU%d", M[i][1]);
+//    }
+//    printf("\n");
+// }
+void printM() {
+   printf("\t");
+   for (int u = 0; u < U; u++) {
+       if (matchU[u] != -1) {
+           printf("V%d ", matchU[u]);
+       }
+   }
+   printf("\n\t");
+   for (int u = 0; u < U; u++) {
+       if (matchU[u] != -1) {
+           printf("|  ");
+       }
+   }
+   printf("\n\t");
+   for (int u = 0; u < U; u++) {
+       if (matchU[u] != -1) {
+           printf("U%d ", u);
+       }
+   }
+   printf("\n");
 }
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+
+/*
+ * Queue Functions
+*/
+
+void printQueue() {
+   printf("Queue:");
+   for (int i = start; i < end; i++) {
+       printf(" %d", queue[i]);
+   }
+   printf("\n");
+}
+
+void enqueue(int x) {
+   queue[end++] = x;
+}
+
+int dequeue() {
+   return queue[start++];
+}
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+
+void MaximumBipartiteMatching() {
+   memset(matchV, -1, sizeof(matchV));
+   memset(matchU, -1, sizeof(matchU));
+   int updated;
+
+   do {
+      start = end = 0;
+       memset(labelV, -1, sizeof(labelV));
+       memset(labelU, -1, sizeof(labelU));
+       updated = 0;
+
+       for (int v = 0; v < V; v++) {
+           if (matchV[v] == -1) {
+               enqueue(v);
+               labelV[v] = -2;
+           }
+       }
+
+       if (start == end) break;
+
+       while (start < end) {
+           printQueue();
+           int w = dequeue();
+
+           if (w < V) {
+               for (int u = 0; u < U; u++) {
+                   if (G[w][u]&& labelU[u] == -1) {
+                       labelU[u] = w;
+                       if (matchU[u] == -1) {
+                           int v = w;
+                           while (v != -2) {
+                               int prev_u = labelU[matchV[v]];
+                               matchU[matchV[v]] = -1;
+                               matchV[v] = u;
+                               matchU[u] = v;
+                               v = labelV[v];
+                               if (v != -2) {
+                                   u = prev_u;
+                               }
+                           }
+                           memset(labelV, -1, sizeof(labelV));
+                           memset(labelU, -1, sizeof(labelU));
+                           start = end = 0;
+                           updated = 1;
+                           printM();
+                           break;
+                       } else {
+                           labelV[matchU[u]] = w;
+                           enqueue(matchU[u]);
+                       }
+                   }
+               }
+               if (updated) break;
+           }
+       }
+   } while (updated);
+}
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+
+void printGraph() {
+   for (int i = 0; i < V; i++) {
+       printf("%d ->", i);
+       for (int j = 0; j < U; j++) {
+           if (G[i][j]) {
+               printf(" %d", G[i][j]); // j
+           }
+       }
+       printf("\n");
+   }
+}
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
 
 int main (int argc, char *argv[])
 {
-   if (argc < 5)
-   {
-      printf("%s 1 2 3 4 5 6 7 8", argv[0]);
-      exit(1);
-   }
-   printf("\n");
-   for (int i = 3; i < argc; i++)
-   {
-      printf("%s ", argv[i]);
-   }
-   printf("\n");
+   if (argc < 5) {
+      printf("%s V U [adjacency lists separated by X]\n", argv[0]);
+      return 1;
+  }
+   
+   V = atoi(argv[1]);
+   U = atoi(argv[2]);
+   memset(G, 0, sizeof(G));
 
-
-   const int n = (argc - 1 - 2);
-   char arr[n];
-   memset(arr, 0, n);
-   for (int i = 0; i < n; i++)
+   int k = 3; 
+   for (int i = 0; i < V; i++)
    {
-      arr[i] = argv[i+3][0];
-      printf("%c ", arr[i]);
+      for (int j = 0; (j < U && k < argc); j++)
+      {
+         if (argv[k][0] == 'X') 
+         {
+            k++;
+            break;
+         }
+         G[i][j] = atoi(argv[k++]);
+      }
    }
-   int x = atoi(argv[1]);
-   int y = atoi(argv[2]);
-   engine(x,y,n,arr);
+
+   printGraph();
+   MaximumBipartiteMatching();
+   return 0;
 }
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
 
 /*
 Note: The input is an adjacenty list delimited by X
@@ -76,24 +234,24 @@ MaximumBipartiteMatching(G)
       printQueue(Q)
       w←Front(Q); Dequeue(Q)
       if w is an element of V
-         for every vertex u adjacent to w do
-            if u is free
+         for every vertex U adjacent to w do
+            if U is free
                //augment
-               M ←M ∪ (w, u)
+               M ←M ∪ (w, U)
                v←w
-               while v is labeled do
-                  u←vertex indicated by v’s label; M ←M − (v, u)
-                  v←vertex indicated by u’s label; M ←M ∪ (v, u)
+               while V is labeled do
+                  u←vertex indicated by v’s label; M ←M − (V, U)
+                  v←vertex indicated by u’s label; M ←M ∪ (V, U)
                remove all vertex labels
                reinitialize Q with all free vertices in V
                break //exit the for loop
-            else //u is matched
-               if (w, u) is not an element of M and u is unlabeled
-                  label u with w
-                  Enqueue(Q, u)
+            else //U is matched
+               if (w, U) is not an element of M and U is unlabeled
+                  label U with w
+                  Enqueue(Q, U)
       else //w is and element of U (and matched)
-         label the mate v of w with w
-         Enqueue(Q, v)
+         label the mate V of w with w
+         Enqueue(Q, V)
       printM(M)
    return M //current matching is maximum
 
